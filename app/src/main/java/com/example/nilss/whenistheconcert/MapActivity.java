6a44945fd17a10e0fdf967ce3955827543f38e76
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2018-10-23 15:47:50 +0200

    Our app now supports Google Maps. Added MapActivity and a function to add pins.

---
+++ app/src/main/java/com/example/nilss/whenistheconcert/MapActivity.java
@@ -0,0 +1,52 @@
+package com.example.nilss.whenistheconcert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private static final String TAG = "MapActivity";
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        initMap();
+    }
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+        LatLng random = new LatLng(55.60587, 13.00073);
+        addMarker(random,  "Malmo");
+        random = new LatLng(57.708870, 11.974560);
+        addMarker(random, "Gothenburg");
+        random = new LatLng(59.334591, 18.063240);
+        addMarker(random, "Stockholm");
+    }
+
+    /**
+     * Puts a marker on the map!
+     * @param latLng, Latlng object containing latitude and longitude.
+     * @param pinDesc, desciptive text when user clicks on pin.
+     */
+    public void addMarker(LatLng latLng, String pinDesc){
+        mMap.addMarker(new MarkerOptions().position(latLng).title(pinDesc));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+}
