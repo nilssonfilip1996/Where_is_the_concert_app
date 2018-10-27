3653f62ef209144d013bd23e232eb593f3a53358
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2018-10-27 09:52:30 +0200

    Added the class TicketMasterHandler with the 2 methods seen in the class diagram.

---
+++ app/src/main/java/com/example/nilss/whenistheconcert/TicketMasterHandler.java
@@ -0,0 +1,27 @@
+package com.example.nilss.whenistheconcert;
+
+import org.json.JSONObject;
+
+public class TicketMasterHandler {
+
+
+    /**
+     * Filip and Jesper will be responsible for this
+     * @param cityName
+     * @return Enough info to place pins on map
+     */
+    public JSONObject getAllEvents(String cityName){
+        //Do something
+        return new JSONObject();
+    }
+
+    /**
+     * Arnes and Zorica will be responsible for this.
+     * @param eventID
+     * @return Up to you guys what info you will need in your activity.
+     */
+    public JSONObject getEventInfo(String eventID){
+        //Do something
+        return new JSONObject();
+    }
+}
