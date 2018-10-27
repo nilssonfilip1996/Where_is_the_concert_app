518c50560ca23a09696e0d309b3d626e6ff9d8cd
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2018-10-27 11:49:24 +0200

    Inner class can(based on URL) fetch a JSONObject from TicketMaster API.

--- app/src/main/java/com/example/nilss/whenistheconcert/MainActivity.java
+++ app/src/main/java/com/example/nilss/whenistheconcert/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test
+        TicketMasterHandler ticketMasterHandler = new TicketMasterHandler();
+        ticketMasterHandler.getAllEvents("malmö");
     }
 }
--- app/src/main/java/com/example/nilss/whenistheconcert/TicketMasterHandler.java
+++ app/src/main/java/com/example/nilss/whenistheconcert/TicketMasterHandler.java
@@ -1,8 +1,23 @@
 package com.example.nilss.whenistheconcert;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TicketMasterHandler {
+    private static final String TAG = "TicketMasterHandler";
+    private final String rootURL = "https://app.ticketmaster.com/discovery/v2/";
+    private final String tmAPIKey = "XztmbuZ4nUUAx2Ki3rG7gDVZzHfGAAbw";
 
 
     /**
@@ -11,8 +26,11 @@ public class TicketMasterHandler {
      * @return Enough info to place pins on map
      */
     public JSONObject getAllEvents(String cityName){
-        //Do something
-        return new JSONObject();
+        //String URL = rootURL + "events.json?" + "classificationName=music"+ "&city=" + cityName + "&apikey="+ tmAPIKey;
+        String URL = rootURL + "events.json?" + "city=" + cityName + "&apikey="+ tmAPIKey;
+        TMRequester tmRequester = new TMRequester();
+        tmRequester.execute(URL);
+        return null;
     }
 
     /**
@@ -23,5 +41,47 @@ public class TicketMasterHandler {
     public JSONObject getEventInfo(String eventID){
         //Do something
         return new JSONObject();
+    }
+
+    private class TMRequester extends AsyncTask<String,Void,String>{
+        @Override
+        protected String doInBackground(String... urls) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(urls[0]);
+                Log.d(TAG, "doInBackground: URL"+ url);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    //Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+                }
+                return buffer.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.d(TAG, "onPostExecute: " + s);
+        }
     }
 }
