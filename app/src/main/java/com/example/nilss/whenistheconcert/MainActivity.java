69270bcf1cffcf10746f0d39b7ec8303ad9d9366
Zorica Ilievska <zorica.dawn23@gmail.com>
2018-10-30 09:02:56 +0100

    Band playing UI

--- app/src/main/java/com/example/nilss/whenistheconcert/MainActivity.java
+++ app/src/main/java/com/example/nilss/whenistheconcert/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.nilss.whenistheconcert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.buttonToBandPlaying);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BandPlayingActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
 
     }
 }
