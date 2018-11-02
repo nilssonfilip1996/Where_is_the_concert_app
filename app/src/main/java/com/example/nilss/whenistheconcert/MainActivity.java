28a21f00da8c4b78ecfab959baf434630569d710
Anas <anasabualsoud@hotmail.com>
2018-11-02 11:21:07 +0100

    Anas commit2

--- app/src/main/java/com/example/nilss/whenistheconcert/MainActivity.java
+++ app/src/main/java/com/example/nilss/whenistheconcert/MainActivity.java
@@ -150,7 +150,6 @@ public class MainActivity extends AppCompatActivity {
                 cityNameRetriever = new CityNameRetriever();
                 cityNameRetriever.execute(location);
 
-
             }
 
             @Override
